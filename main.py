cffc9a601e40660325c97fcf62775335b4506180
Raul Silveira <raul95@gmail.com>
2017-11-05 18:01:09 -0300

    [Menu] Itens

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ done = False
 
 fontKanji = pygame.font.Font("./Fonts/Kengo.ttf", 62)
 fontCardgame = pygame.font.Font("./Fonts/Kengo.ttf", 32)
-fontItens = pygame.font.Font("./Fonts/Kengo.ttf", 20)
+fontItens = pygame.font.SysFont("monospace", 25)
 
 kanjiName = fontKanji.render("Kanji", 1, (255,69,0))
 cardgameName = fontCardgame.render("Cardgame", 1, (255,215,0))
@@ -21,10 +21,10 @@ sairName = fontItens.render("Exit", 1, (255,255,255))
 screen.blit(kanjiName, (325, 50))
 screen.blit(cardgameName, (315, 125))
 screen.blit(jogarName, (20, 250))
-screen.blit(instrucoesName, (20, 270))
-screen.blit(configuracoesName, (20, 290))
-screen.blit(sobreName, (20, 310))
-screen.blit(sairName, (20, 330))
+screen.blit(instrucoesName, (20, 280))
+screen.blit(configuracoesName, (20, 310))
+screen.blit(sobreName, (20, 340))
+screen.blit(sairName, (20, 360))
 
 kanjiKanji = pygame.image.load('./Kanjis/kanji.png')
 screen.blit(kanjiKanji, (450, 225))

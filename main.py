a2900ba5c8331e837850d48fdd790ba0a2acfbcb
Raul Silveira <raul95@gmail.com>
2017-11-05 16:46:13 -0300

    [Menu] Game name

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ pygame.display.set_caption('Kanji - Cardgame')
 screen = pygame.display.set_mode((795, 411))
 done = False
 
+font = pygame.font.SysFont("monospace", 15)
+
+gameName = font.render("Kanji Cardgame", 12, (255,255,255))
+screen.blit(label, (400, 100))
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
